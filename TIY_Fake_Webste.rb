b156df582d26887eb2f245a9cdc9466dfde95b88
Matt Fair <matt.j.fair@gmail.com>
2015-10-26 15:41:40 -0400

    Added an each loop to add the 'other products' section and title is dynamic based on URL

--- TIY_Fake_Webste.rb
+++ TIY_Fake_Webste.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
 
-get "/"  do
-  links=[{link:"link1"},{link:"link2"},{link:"link3"}, {link:"Link4"}, {link:"Link5"}]
-  erb :index, :locals => {:links => links}
-end
+
 
 get "/:title"  do
   links=[{link:"link1"},{link:"link2"},{link:"link3"}, {link:"Link4"}, {link:"Link5"}]
-  erb :index, :locals => {:links => links}
+
+  products=[{class: "product1", text:"HOMES"},{class: "product2", text:"BOYFIGHTS"},{class: "product3", text:"BACKYARD BOYFIGHTS"},{class: "product4", text:"BACKSEAT BOYFIGHTS"}]
+
+  erb :index, :locals => {links:links, products:products}
 end
